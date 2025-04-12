c6c0c41c65b46972cbd5d4429bac83698635de72
Karafakeu <karafak7@gmail.com>
2025-04-12 13:08:03 +0200

    synch

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='decotrace',
+      version='0.1',
+      description='A decorator based, trace-like debugger for python',
+      url='https://github.com/Karafakeu/DecoTrace',
+      author='Jakub Kubin',
+      author_email='jakubin07@gmail.com',
+      license='MIT',
+      packages=['decotrace'],
+      zip_safe=False)
